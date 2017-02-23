2358a7c5a79853f74250882cfd5bf6a09e9d2793
jwmunn <contact@justinmunn.co>
2017-02-23 08:55:01 -0800

    Missing number codewar problem.

---
+++ missing-number.rb
@@ -0,0 +1,11 @@
+def find_missing_number(sequence)
+  #your code here
+end
+
+
+
+find_missing_number("1 2 3 5") # 4,"It must work for missing middle terms"
+find_missing_number("1 3") # 2,"It must work for missing middle terms"
+find_missing_number("") # 0,"It must return 0 for an empty sequence"
+find_missing_number("2 3 4 5") # 1,"It must return 1 for a sequence missing the first element"
+find_missing_number("2 1 4 3 a") # 1,"It must return 1 for an invalid sequence"
