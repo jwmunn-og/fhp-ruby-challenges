df4cbcabe4b4032cd5df15ea821c88d48211b965
jwmunn <contact@justinmunn.co>
2017-02-25 10:50:36 -0800

    Made method to count images.

--- url-scan.rb
+++ url-scan.rb
@@ -1,7 +1,12 @@
-require "open-uri"
-url = "http://justinmunn.co"
-pattern = "<img"
+# Folling examples in http://ruby.bastardsbook.com/chapters/methods/
 
-page = open(url).read
-tags = page.scan(pattern)
-puts "The site #{url} has #{tags.length} img tags"
+require "open-uri"
+def count_image_tags(url)
+  pattern = "<img"
+  page = open(url).read
+  tags = page.scan(pattern)
+  puts "The site #{url} has #{tags.length} img tags"
+end
+
+count_image_tags("http://justinmunn.co")
+count_image_tags("http://www.thefirehoseproject.com/")
