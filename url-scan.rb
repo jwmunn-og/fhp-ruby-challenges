2472fb7b9690bf4e68e400df931cb39468832cbc
jwmunn <contact@justinmunn.co>
2017-02-25 11:10:00 -0800

    Made methods more modular.

--- url-scan.rb
+++ url-scan.rb
@@ -1,16 +1,24 @@
 # Folling examples in http://ruby.bastardsbook.com/chapters/methods/
 
 require "open-uri"
-def count_html_tags(url, tag)
-  pattern = /<#{tag}\b/
-  page = open(url).read
-  tags = page.scan(pattern)
-  puts "The site #{url} has #{tags.length} #{tag} tags"
+def fetch_page(url)
+  return open(url).read
 end
 
-url = "http://justinmunn.co"
+def count_html_tags(page, tag)
+  pattern = /<#{tag}\b/
+  tags = page.scan(pattern)
+  return tags.length
+end
+
+sites = ["http://justinmunn.co", "http://www.thefirehoseproject.com/", "https://www.codewars.com"]
 tags = ["a", "div", "img"]
 
-tags.each do |tag|
-  count_html_tags(url, tag)
+sites.each do |url|
+  puts "#{url} has:"
+  tags.each do |tag|
+    page = fetch_page(url)
+    tag_count = count_html_tags(page, tag)
+    puts "\t - #{tag_count} <#{tag}> tags"
+  end
 end
