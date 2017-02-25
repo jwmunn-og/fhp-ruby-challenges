57c3c4b6bb8e0541f3b010e5b5304a9fd7ddb6c6
jwmunn <contact@justinmunn.co>
2017-02-25 10:53:14 -0800

    Altered method to count any html tag

--- url-scan.rb
+++ url-scan.rb
@@ -1,12 +1,12 @@
 # Folling examples in http://ruby.bastardsbook.com/chapters/methods/
 
 require "open-uri"
-def count_image_tags(url)
-  pattern = "<img"
+def count_html_tags(url, tag)
+  pattern = /<#{tag}\b/
   page = open(url).read
   tags = page.scan(pattern)
-  puts "The site #{url} has #{tags.length} img tags"
+  puts "The site #{url} has #{tags.length} #{tag} tags"
 end
 
-count_image_tags("http://justinmunn.co")
-count_image_tags("http://www.thefirehoseproject.com/")
+count_html_tags("http://justinmunn.co", "img")
+count_html_tags("http://www.thefirehoseproject.com/", "div")
