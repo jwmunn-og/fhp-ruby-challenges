930694cb758b993887fe9a7303d7a68ffa0f336c
jwmunn <contact@justinmunn.co>
2017-02-26 13:05:26 -0800

    Works for edge cases

--- image-blur#2.rb
+++ image-blur#2.rb
@@ -2,8 +2,7 @@ class Image
   def initialize(image_data)
     @image_data = image_data
   end
-
-
+  
   def blur_image
     coords = []
     @image_data.each_with_index do |row, row_idx|
@@ -16,11 +15,19 @@ class Image
       end
     end
 
-    coords.each do |x|
-      @image_data[x[0]][x[1]-1] = 1
-      @image_data[x[0]][x[1]+1] = 1
-      @image_data[x[0]-1][x[1]] = 1
-      @image_data[x[0]+1][x[1]] = 1
+    coords.each do |row, col|
+      if col != 0
+        @image_data[row][col-1] = 1
+      end
+      if col != @image_data[0].length - 1
+        @image_data[row][col+1] = 1
+      end
+      if row != 0
+        @image_data[row-1][col] = 1
+      end
+      if row != @image_data.length - 1
+        @image_data[row+1][col] = 1
+      end
     end
     
     # puts coords.inspect
@@ -56,11 +63,11 @@ two_px_transform = Image.new([
 
 px_edge_transform = Image.new([
   [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
+  [0, 0, 0, 1],
   [1, 0, 0, 0],
-  [0, 0, 0, 0]
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 1, 0]
 ])
 
 px_edge_transform.output_image
