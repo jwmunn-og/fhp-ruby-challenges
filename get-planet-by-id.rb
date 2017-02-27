d8b7e1421fd54857ff4d42597c5b7a158c23cc30
jwmunn <contact@justinmunn.co>
2017-02-26 23:30:20 -0800

    Solution for refactored greeting

--- get-planet-by-id.rb
+++ get-planet-by-id.rb
@@ -1,14 +1,3 @@
 def get_planet_name(id)
-  name = ''
-  case id
-    when 1; name = "Mercury"
-    when 2; name = "Venus"
-    when 3; name = "Earth"
-    when 4; name = "Mars"
-    when 5; name = "Jupiter"
-    when 6; name = "Saturn"
-    when 7; name = "Uranus"  
-    when 8; name = "Neptune"
-  end
-  return name
+  %w[Mercury Venus Earth Mars Jupiter Saturn Uranus Neptune][id - 1]
 end
---
+++ refactored-greeting.rb
@@ -0,0 +1,14 @@
+class Person
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+  
+  def greet(yourName)
+    "Hello #{yourName}, my name is #{self.name}"
+  end
+end
+
+p joe = Person.new('Joe')
+p joe.greet('Kate') # should return 'Hello Kate, my name is Joe'
+p joe.name # should == 'Joe'
