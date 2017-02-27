9ac855c315401fdaeb1264456787cb25d9d7ac03
jwmunn <contact@justinmunn.co>
2017-02-26 22:23:14 -0800

    Refactored grader solution.

--- grader.rb
+++ grader.rb
@@ -1,9 +1,9 @@
 def grader(score)
   case score
-    when (0.9..1) then "A"
-    when (0.8..0.9) then "B"
-    when (0.7..0.8) then "C"
-    when (0.6..0.7) then "D"
+    when 0.9..1; "A"
+    when 0.8..0.9; "B"
+    when 0.7..0.8; "C"
+    when 0.6..0.7; "D"
     else "F"
   end
   
