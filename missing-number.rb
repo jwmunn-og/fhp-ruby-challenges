d8d69e99a07793c20ffb1e0b440df6f8d1fe6d6e
jwmunn <contact@justinmunn.co>
2017-02-26 20:55:20 -0800

    Remove duplicates without .unique

--- missing-number.rb
+++ missing-number.rb
@@ -30,3 +30,15 @@ end
 # find_missing_number("2 3 4 5") # 1,"It must return 1 for a sequence missing the first element"
 # find_missing_number("2 1 4 3 a") # 1,"It must return 1 for an invalid sequence"
 find_missing_number("1 2 3 4 5") # 1,"It must return 0 for good sequence"
+
+
+# Solution on Codewars to discuss with mentor
+def find_missing_number(sequence)
+  number_sequence = sequence.split.map(&:to_i).sort
+  
+  number_sequence.each.with_index(1) do |actual, expected|
+    return expected unless actual == expected
+  end
+  
+  0
+end
