ff1c4462d1f3e0b6c6c0167e341b6930ff44186f
jwmunn <contact@justinmunn.co>
2017-03-01 22:43:17 -0800

    More lambda examples.

--- js/script.rb
+++ js/script.rb
@@ -1,18 +1,13 @@
-def add_numbers(num1,num2)
+add_numbers = lambda do |num1, num2|
   return num1 + num2
 end
 
-def subtract_numbers(num1, num2)
+subtract_numbers = lambda do |num1, num2|
   return num1 - num2
 end
 
-add_numbers2 = lambda do |num1, num2|
-  return num1 + num2
+def combine(m, num1, num2)
+  return m.call(num1, num2)
 end
 
-subtract_numbers2 = lambda do |num1, num2|
-  return num1 - num2
-end
-
-puts subtract_numbers(3,4)
-puts subtract_numbers2.call(3,4)
+puts combine(subtract_numbers, 3, 4)
