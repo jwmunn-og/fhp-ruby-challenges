e7e6ec30a8324f20b515e74a8887dc03f98b429c
jwmunn <contact@justinmunn.co>
2017-03-10 21:12:40 -0800

    JSON conversion in ruby.

--- js/data.rb
+++ js/data.rb
@@ -1,3 +1,5 @@
+require 'active_support/all'
+
 num = 2
 hash = {
   1 => "One",
@@ -5,4 +7,11 @@ hash = {
   3 => "Three"
 }
 
-puts hash[num]
+# puts hash[num]
+json_string = '{"1":"One","2":"Two","3":"Three"}'
+
+parsed = ActiveSupport::JSON.decode(json_string)
+puts parsed.inspect
+
+puts parsed[1].inspect
+puts hash[1].inspect
