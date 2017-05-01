e8762ef563f4634b8e1992895bc0f08850d35369
jwmunn <contact@justinmunn.co>
2017-04-30 16:33:20 -0800

    Refactored Luhn Algorithm

--- luhn-algorithm/lib/luhn.rb
+++ luhn-algorithm/lib/luhn.rb
@@ -4,51 +4,22 @@ module Luhn
     reversed_digits = digits.reverse
     double = []
 
-    puts "Split:"
-    p digits
-
-    puts "Reverse:"
-    p reversed_digits
-
+    # Double every other in reverse order
     reversed_digits.each_with_index.map do |digit, index|
-      if (index + 1).even?
-        double << digit * 2
-      else
-        double << digit
-      end
+      (index + 1).even? ? double << digit * 2 : double << digit
     end
-    puts "Double:"
-    p double
-
-    puts "Reverse Double:"
+   
+    # Un-reverse
     reversed_double = double.reverse
-    p reversed_double
 
     reversed_double.map! do |n|
-      if n >= 10
-        n - 9
-      else
-        n
-      end
+      n >= 10 ? n - 9 : n
     end
 
-    puts "Minus 9 if over 10:"
-    p reversed_double
-
-    puts "Sum:"
     arr_sum = reversed_double.inject(:+)
-    p  arr_sum
 
-    puts "Valid?:"
     return arr_sum % 10 == 0    
 
   end
 end
 
-p Luhn.is_valid?(4194560385008504)
-puts "---"
-p Luhn.is_valid?(4194560385008505)
-puts "---"
-p Luhn.is_valid?(377681478627336)
-puts "---"
-p Luhn.is_valid?(377681478627337)
